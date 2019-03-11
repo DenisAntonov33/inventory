91cc71ca1783fed584ecde3b13417be7b3fee526
Sergey Perezhogin <klikkn@yandex.ru>
2019-03-11 20:36:30 +0500

    Initial

---
+++ electron/index.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  const isDev = process.env.NODE_ENV === "development";
+
+  console.log("isdev", isDev);
+
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+
+  if (isDev) mainWindow.webContents.openDevTools();
+
+  mainWindow.on("closed", function() {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", () => {
+  createWindow();
+});
+
+app.on("window-all-closed", function() {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function() {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
