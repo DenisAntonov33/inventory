af717a5689cde4f882a352cc2890fd11c91708cd
Sergey Perezhogin <klikkn@yandex.ru>
2019-04-18 10:48:25 +0500

    added header

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./index.scss";
 import Modal from "@material-ui/core/Modal";
 import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
 
 class SimpleModal extends React.Component {
   render() {
@@ -15,7 +16,12 @@ class SimpleModal extends React.Component {
         aria-labelledby="customized-dialog-title"
         open={isOpen}
       >
-        <Paper className="modal__container">{children}</Paper>
+        <Paper className="modal__container">
+          <div className="modal__header">
+            <Button onClick={onClose}>Close</Button>
+          </div>
+          <div className="modal__content">{children}</div>
+        </Paper>
       </Modal>
     );
   }
